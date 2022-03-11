6777136f2f167fd8bb89abe97778d5204bdf19ab
Jorge Paul <jserranopaul@gmail.com>
2022-03-11 11:03:02 +0100

    Router scaffold

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Layout from './components/Layout';
+
 const App = () => {
-  return <div>Hello there</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<div>All posts</div>} />
+        <Route path='new-post' element={<div>Create post</div>} />
+        <Route path='about' element={<div>About</div>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/components/Layout.js
@@ -0,0 +1,24 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/new-post'>Create post</NavLink>
+          </li>
+          <li>
+            <NavLink to='/about'>About</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
